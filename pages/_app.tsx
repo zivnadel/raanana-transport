f671d27c6b0c749eedeb975a309b4f5279dbff7b
zivnadel <zywn1414@gmail.com>
2022-07-10 00:04:28 +0300

    Added an Error Boundary component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Navbar from '../components/Navbar'
 import Head from 'next/head'
+import ErrorBoundary from '../components/ErrorBoundary'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ErrorBoundary>
       <Head>
         <title>הסעות רעננה</title>
       </Head>
@@ -15,7 +16,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <main>
         <Component {...pageProps} />
       </main>
-    </>
+    </ErrorBoundary>
   )
 }
 
