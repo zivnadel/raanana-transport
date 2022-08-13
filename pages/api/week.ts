bea6d22b29ad6598ee44d0cb3b7b3913eb6fd194
zivnadel <zywn1414@gmail.com>
2022-08-14 02:33:18 +0300

    Finished update week schedule, the dashboard is now finished

--- pages/api/week.ts
+++ pages/api/week.ts
@@ -1,9 +1,13 @@
 import Cors from "cors";
-import { AnyBulkWriteOperation, Document } from "mongodb";
+import { AnyBulkWriteOperation, Db, Document } from "mongodb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { unstable_getServerSession } from "next-auth";
 import clientPromise from "../../lib/mongodb";
+import DateObjectType, { busType } from "../../types/DateObjectType";
 import DayObjectType from "../../types/DayObjectType";
+import PricesObjectType from "../../types/PricesObjectType";
+import PupilObjectType from "../../types/PupilObjectType";
+import { calculateBusType, calculatePrice } from "../../utils/dateUtils";
 import { authOptions } from "./auth/[...nextauth]";
 
 const cors = Cors({
@@ -50,31 +54,143 @@ const getDays = async (day?: number) => {
 	}
 };
 
-const updateWeek = async (data: string) => {
-	const week = JSON.parse(data) as DayObjectType[];
+const populateDate = (
+	date: DateObjectType,
+	pupils: PupilObjectType[],
+	prices: PricesObjectType
+) => {
+	for (let pupil of pupils) {
+		const scheduleDay = pupil.schedule.find((day) => day.day === date.day);
+		if (scheduleDay) {
+			for (let hour of scheduleDay.hours) {
+				const indexHour: keyof typeof date.transportations =
+					hour as keyof typeof date.transportations;
+				if (date.transportations[indexHour]?.pupils) {
+					date.transportations[indexHour]?.pupils.push(pupil.name);
+				} else {
+					date.transportations[indexHour] = {
+						pupils: [pupil.name],
+						price: 0,
+						busType: [],
+					};
+				}
+			}
+		}
+	}
+	let hour: keyof typeof date.transportations;
+	for (hour in date.transportations) {
+		let busTypes: busType[];
+		if (hour === "morning") {
+			busTypes = [busType.morning];
+		} else {
+			busTypes = calculateBusType(date.transportations[hour]!.pupils.length);
+		}
+		date.transportations[hour]!.busType = busTypes;
+		date.transportations[hour]!.price = calculatePrice(busTypes, prices);
+	}
+};
 
-	const db = (await clientPromise).db();
+const updateSchedule = async (week: DayObjectType[], db: Db) => {
+	try {
+		const daysArray = week.map((day) => day.day);
 
-	const bulkData: AnyBulkWriteOperation<DayObjectType | Document>[] = week.map(
-		(day) => {
-			return {
+		let datesToUpdate = await db
+			.collection<DateObjectType>("dates")
+			.find({ day: { $in: daysArray } })
+			.toArray();
+
+		const hours: ("morning" | "15:30" | "17:00")[] = [
+			"morning",
+			"15:30",
+			"17:00",
+		];
+
+		let pupils: PupilObjectType[] | null = null;
+		let prices: PricesObjectType | null = null;
+
+		let bulkData: AnyBulkWriteOperation<DateObjectType | Document>[] = [];
+
+		for (let date of datesToUpdate) {
+			for (let weekDay of week) {
+				if (date.day === weekDay.day) {
+					for (let hour of hours) {
+						if (!weekDay.hours.includes(hour) && date.transportations[hour]) {
+							// need to remove hour
+							delete date.transportations[hour];
+						}
+						if (weekDay.hours.includes(hour) && !date.transportations[hour]) {
+							// need to add hour
+							if (!pupils) {
+								pupils = await db
+									.collection<PupilObjectType>("pupils")
+									.find({})
+									.toArray();
+							}
+
+							if (!prices) {
+								prices = await db
+									.collection<PricesObjectType>("prices")
+									.findOne({});
+							}
+
+							populateDate(date, pupils, prices!);
+						}
+					}
+				}
+			}
+			bulkData.push({
 				updateOne: {
-					filter: { day: day.day },
+					filter: { _id: date._id },
 					update: {
 						$set: {
-							hours: day.hours,
+							transportations: date.transportations,
+							totalAmount:
+								(date.transportations.morning?.price || 0) +
+								(date.transportations["15:30"]?.price || 0) +
+								(date.transportations["17:00"]?.price || 0),
 						},
 					},
 				},
-			};
+			});
 		}
-	);
 
-	const response = await db.collection("week").bulkWrite(bulkData);
+		const response = await db.collection("dates").bulkWrite(bulkData);
+		return response;
+	} catch (error: any) {
+		throw new Error(error);
+	}
+};
 
-	// TODO: Update date collection with new / removed data.
+const updateWeek = async (data: string) => {
+	try {
+		const week = JSON.parse(data) as DayObjectType[];
 
-	return response;
+		const db = (await clientPromise).db();
+
+		const bulkData: AnyBulkWriteOperation<DayObjectType | Document>[] =
+			week.map((day) => {
+				return {
+					updateOne: {
+						filter: { day: day.day },
+						update: {
+							$set: {
+								hours: day.hours,
+							},
+						},
+					},
+				};
+			});
+
+		let response = await db.collection("week").bulkWrite(bulkData);
+
+		if (response.isOk()) {
+			response = await updateSchedule(week, db);
+		}
+
+		return response;
+	} catch (error: any) {
+		throw new Error(error);
+	}
 };
 
 export default async function handler(
